2973495f8d678e6303293a38f6b8299db01b1ad4
Forest Hu <foresthu2006@gmail.com>
2018-07-08 22:19:39 -0700

    Reformat

--- Obfuscation.java
+++ Obfuscation.java
@@ -8,7 +8,31 @@ public class Obfuscation {
     //what does the longest version of the string in line 18 contain after a call of the following method, provided that it does not recurse?
     //the code has been edited so that it will not compile properly to prevent people from editing the code and running it.
     Obfuscate.obfuscate obfuscate(Obfuscate obfuscate) {
-        return obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate) + obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate), obfuscate.obfuscate(obfuscate).Obfuscate("obfuscat3 "), obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate.obfuscate(obfuscate))))); //what does it do?!?!?1
+        return obfuscate(
+          obfuscate
+            .obfuscate(obfuscate)
+            .obfuscate(
+              obfuscate
+                .obfuscate(obfuscate)
+                .obfuscate(obfuscate) +
+              obfuscate
+                .obfuscate(obfuscate)
+                .obfuscate(
+                  obfuscate
+                    .obfuscate(obfuscate)
+                    .Obfuscate(obfuscate),
+                  obfuscate
+                    .obfuscate(obfuscate)
+                    .Obfuscate("obfuscat3 "),
+                  obfuscate
+                    .obfuscate(obfuscate)
+                    .Obfuscate(
+                      obfuscate
+                        .obfuscate(obfuscate)
+                    )
+                )
+            )
+        ); //what does it do?!?!?1
     }
     
 }
